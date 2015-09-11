eacd84f86a0ae62878788b9527a94ca5aab389f7
psadeghian <pasha1129@gmail.com>
2015-09-11 23:32:38 +0000

    I finished part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,37 @@
 
 def sum arr
   # YOUR CODE HERE
+    sum = 0
+    arr.each do |n|
+        sum = sum + n
+    end
+    return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+    return 0 if arr.size == 0
+    return arr[0] if arr.size == 1
+    sum = arr.max
+    arr.delete_at(arr.find_index(sum))
+    sum = sum + arr.max
+    return sum
+    # or use sorting and reversing
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.size < 2
+  first = arr[0]
+  arr.delete_at(0)
+  while arr.size != 0
+    arr.each do |num|
+      sum = first + num
+      return true if n == sum
+    end
+    first = arr[0]
+    arr.delete_at(0)
+  end
+  return false
 end
 
 # Part 2
